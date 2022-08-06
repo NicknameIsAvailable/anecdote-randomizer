b62e6dc0d160468c63f4df76317b3339b101064b
NicknameIsAvailable <iamgenii@yandex.ru>
2022-08-06 14:44:17 +0300

    деплой на верселе

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import {Button, Container} from "@mui/material";
-import "./index.css"
+import {Button, Container, TextField} from "@mui/material";
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -21,12 +20,23 @@ function App() {
     <Container className="App">
       <h1>Рандомайзер анекдотов</h1>
         <AnecdoteField text={visibleAnecdote}/>
-        <Button  variant="contained" onClick={() => {
+      <div>
+        <Button  variant="contained" style={{marginTop: "16px", marginRight: "16px"}} onClick={() => {
           setVisibleAnecdote([anecdotes[random]])
         }} className="Button">Новый анекдот</Button>
-        <Button  variant="contained" style={{margin: "16px"}} onClick={speak} className="button"><VolumeDownIcon/></Button>
+        <Button  variant="contained" style={{marginTop: "16px", marginRight: "16px"}} onClick={speak} className="button"><VolumeDownIcon/></Button>
+      </div>
 
-      <p>Я знаю, что это выглядит, как говно.</p>
+      {/*<div>*/}
+      {/*  <TextField*/}
+      {/*      style={{marginTop: "16px", width: "100%"}}*/}
+      {/*      placeholder="Введите свой анекдот"*/}
+      {/*      minRows={5}*/}
+      {/*      maxRows={10}*/}
+      {/*      />*/}
+      {/*</div>*/}
+      {/*  <Button  variant="outlined" style={{marginTop: "16px", marginRight: "16px"}} onClick={speak} className="button">Добавить свой анекдот</Button>*/}
+        <h2>Здесь есть {anecdotes.length} анекдотов.</h2>
     </Container>
   );
 }
